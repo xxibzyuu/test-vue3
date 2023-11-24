f060913af84543708904e6ab54765bdb82956e47
Zyuu <13413629157@163.com>
2023-11-24 15:00:32 +0800

    feat: 推荐页

--- pages/recommend/recommend.vue
+++ pages/recommend/recommend.vue
@@ -1,5 +1,200 @@
 <template>
   <view>
-    recommend
+    <view class="view-header">
+      <image class="view-header-image" :src="banner">
+        <!-- src="http://39.108.155.236:20006/userfiles/1/files/mediaResource/2023/03/1.jpg" -->
+      </image>
+    </view>
+    <view class="view-content">
+      <view class="view-content-form">
+        <uni-forms border label-position="top" label-width="400rpx">
+          <uni-forms-item label="企业名称">
+            <input
+              placeholder="请输入企业名称"
+              class="view-content-form-input"
+              v-model="formData.company"
+            />
+          </uni-forms-item>
+          <uni-forms-item label="客户姓名" required>
+            <input
+              placeholder="请输入客户姓名"
+              class="view-content-form-input"
+              v-model="formData.name"
+            />
+          </uni-forms-item>
+          <uni-forms-item label="客户手机" required>
+            <view>
+              <!-- <zj-phone-input @getNumber="getNumber"  ref='phonenum'/> -->
+            </view>
+          </uni-forms-item>
+          <uni-forms-item label="报备项目" required>
+            <uni-data-picker
+              placeholder="请选择报备项目"
+              popup-title="请选择报备项目"
+              :localdata="PROJECR_LIST"
+              :border="false"
+              :clear-icon="false"
+              v-model="formData.selectedProject"
+            >
+            </uni-data-picker>
+          </uni-forms-item>
+
+          <uni-forms-item label="需求产品">
+            <uni-data-picker
+              placeholder="请选择需求产品"
+              popup-title="请选择需求产品"
+              :localdata="PRODUCT_LIST"
+              :border="false"
+              :clear-icon="false"
+              v-model="formData.selectedProduct"
+            >
+            </uni-data-picker>
+          </uni-forms-item>
+          <uni-forms-item label="需求面积">
+            <uni-data-picker
+              placeholder="请选择需求面积"
+              popup-title="请选择需求面积"
+              :localdata="AREA_LIST"
+              :border="false"
+              :clear-icon="false"
+              v-model="formData.selectedArea"
+            >
+            </uni-data-picker>
+          </uni-forms-item>
+
+          <uni-forms-item label="现场招商接待" required>
+            <uni-data-picker
+              placeholder="请选择现场招商接待"
+              popup-title="请选择现场招商接待"
+              :localdata="CLERK_LIST"
+              :border="false"
+              :clear-icon="false"
+              v-model="formData.selectedClerk"
+            >
+            </uni-data-picker>
+          </uni-forms-item>
+          <!-- <uni-forms-item label="预计到访时间" required>
+						<view  class="filter-item-last-box">
+							<uni-datetime-picker ref="VisitTime" type="datetime" :border="false" v-model="selectedVisitTime" 
+							@change="changeLog" :start="datestart" hide-second="true" :clear-icon="false"/>
+							
+							<view class="filter-item-last-box-info" @click="toggleVisitTime">
+									<view class="filter-item-last-box-infoitem">
+									</view>							
+							</view>
+						</view>
+					</uni-forms-item> -->
+        </uni-forms>
+      </view>
+      <view>
+        <button
+          @click="submitRecommend"
+          style="width: 100%; position: fixed; bottom: 0"
+          :disabled="disabledRecommend"
+          type="default"
+          class="primary-btn"
+          hover-class="btn-recommend-hover"
+        >
+          提交报备
+        </button>
+      </view>
+    </view>
   </view>
 </template>
+
+<script setup>
+import { ref, inject, reactive } from "vue";
+import { onLoad } from "@dcloudio/uni-app";
+import {
+  PROJECR_LIST,
+  PRODUCT_LIST,
+  AREA_LIST,
+  CLERK_LIST,
+} from "../../static/js/dist.js";
+const $request = inject("request");
+const imageHost = inject("imageHost");
+
+onLoad(() => {
+  getBannerImage();
+});
+
+// 获取banner
+let banner = ref("");
+function getBannerImage() {
+  $request
+    .request({
+      url: "/front/getBanners",
+      data: {
+        type: "recommend",
+      },
+    })
+    .then((res) => {
+      let data = res.data;
+
+      if (data && data.length > 0) {
+        banner = imageHost + data[0].mediaThumbnail;
+      }
+    });
+}
+
+//
+let formData = reactive({
+  company: "",
+  name: "",
+  selectedProject: "",
+  selectedProduct: "",
+  selectedArea: "",
+  selectedClerk: "",
+});
+function submitRecommend() {
+  console.log(formData);
+}
+</script>
+
+<style lang="scss" scoped>
+.view-header {
+  z-index: -1;
+  position: relative;
+  width: 750rpx;
+  height: 456rpx;
+}
+.view-header-image {
+  width: 100%;
+  display: inline-block;
+}
+.view-content {
+  border-radius: 40rpx 40rpx 0rpx 0rpx;
+  margin-top: -50rpx;
+  z-index: 99;
+  background-color: white;
+  // text-align: center;
+}
+.view-content-form {
+  padding: 40rpx 40rpx 180rpx 40rpx;
+}
+.view-content-form-button {
+  margin: 0rpx 10rpx 20rpx 10rpx;
+}
+.view-content-form-input {
+  width: auto;
+  position: relative;
+  overflow: hidden;
+  flex: 1;
+  line-height: 1;
+  font-size: 26rpx;
+  height: 35px;
+}
+.view-content-form-textarea {
+  position: relative;
+  overflow: hidden;
+  flex: 1;
+  line-height: 1.5;
+  font-size: 26rpx;
+  // margin: 6px;
+  margin-left: -10rpx;
+  margin-right: -10rpx;
+  height: 80px;
+  min-height: 80px;
+  width: auto;
+}
+</style>
---
+++ static/js/dist.js
@@ -0,0 +1,105 @@
+export const PRODUCT_LIST = [
+  {
+    id: "c6ac6890940b45eab85a69988e016792",
+    value: "0",
+    label: "高层厂房",
+    type: "property_type",
+  },
+  {
+    id: "350766b47d674b8ea5a7ed7d7a10bfa4",
+    value: "1",
+    label: "独栋厂房",
+    type: "property_type",
+  },
+  {
+    id: "d6362f9c75394ce392a29ad07fc3e22a",
+    value: "2",
+    label: "研发办公",
+    type: "property_type",
+  },
+  {
+    id: "15ff8c94aa474aba84902764980ea5c4",
+    value: "3",
+    label: "钢构",
+    type: "property_type",
+  },
+  {
+    id: "f5bbd8dd8867462b84e553d6c3f89c4e",
+    value: "4",
+    label: "宿舍公寓",
+    type: "property_type",
+  },
+  {
+    id: "2d32ae6ee08f4ca2a79354c3f5275631",
+    value: "5",
+    label: "写字楼",
+    type: "property_type",
+  },
+  {
+    id: "4f42323a92b3465ab9cd6a33fd289ecf",
+    value: "6",
+    label: "其他",
+    type: "property_type",
+  },
+];
+export const AREA_LIST = [
+  {
+    id: "70d6a7c83a0f41d59523b31f81a6032e",
+    value: "0,500",
+    label: "<500㎡",
+    type: "area_section",
+  },
+  {
+    id: "7fb6069cd8e842aaa7f0aa96582def7a",
+    value: "500,1000",
+    label: "500-1000㎡",
+    type: "area_section",
+  },
+  {
+    id: "0e017168bbe34a8fbff329b1e02b2c7d",
+    value: "1000,2000",
+    label: "1000-2000㎡",
+    type: "area_section",
+  },
+  {
+    id: "ea0bbbc838a24a9fa3d8d26eb46b815e",
+    value: "2000,4000",
+    label: "2000-4000㎡",
+    type: "area_section",
+  },
+  {
+    id: "1c8f326c237c4d1b9fab8ebdf582ed04",
+    value: "4000,",
+    label: ">4000㎡",
+    type: "area_section",
+  },
+];
+export const CLERK_LIST = [
+  { phone: "15010231915", name: "吴贤兵", id: "24766" },
+  { phone: "18666663908", name: "熊建斌", id: "24767" },
+  { phone: "17688934126", name: "谢羊城", id: "24760" },
+  { phone: "13725268587", name: "游文增", id: "24774" },
+];
+
+export const PROJECR_LIST = [
+  { id: "21", name: "广州黄埔中集大湾区国际人才创业园" },
+  { id: "41", name: "广州黄埔中集生命健康创新园" },
+  { id: "68", name: "东莞松山湖中集智谷" },
+  { id: "70", name: "长沙高新中集瑞芯企业广场" },
+  { id: "55", name: "东莞松山湖中集数字科技产业园" },
+  { id: "67", name: "深圳宝安中集创谷产业园" },
+  { id: "69", name: "上海浦东中集临港中心" },
+  { id: "42", name: "深圳光明中集低轨卫星物联网产业园" },
+  { id: "71", name: "深圳宝安中集空天园" },
+  { id: "72", name: "扬州邗江中集文昌商业中心" },
+  { id: "43", name: "佛山南海中集智谷" },
+  { id: "44", name: "佛山顺德中集大良数科城" },
+  { id: "50", name: "佛山顺德中集智城" },
+  { id: "51", name: "佛山顺德中集智谷" },
+  { id: "52", name: "曲靖麒麟中集麒麟国际中心" },
+  { id: "53", name: "深圳南山中集太子湾" },
+  { id: "54", name: "扬州江都中集江广中心" },
+  { id: "10", name: "昊楼1" },
+  { id: "57", name: "东莞东城中集国际数科城" },
+  { id: "75", name: "人才园客户导入测试" },
+];
